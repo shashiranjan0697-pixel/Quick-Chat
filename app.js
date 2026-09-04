9a18985bc2b1e9cfe846c50a427932e943d661d2
shashiranjan0697 <shashiranjan0697@gmail.com>
2026-09-04 17:10:56 +0530

    first Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const express = require("express");
+require("dotenv").config();
+
+const connectDB = require("./config/database");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+}); 
---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully");
+    } catch (err) {
+        console.log("Connection failed.");
+        console.log("Error:", err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB; 
